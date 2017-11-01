4ce7ae82c67780671229f501ee461cb45a0d37ca
Charity Youngblood <youngblood.charity@gmail.com>
2017-11-01 18:54:48 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -11,7 +11,7 @@ def speak_to_grandma(string)
 # and yells back
 # NO, NOT SINCE 1938!
 elsif string == string.upcase
-  puts "NO, NOT SINCE 1938!"
+  return "NO, NOT SINCE 1938!"
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
