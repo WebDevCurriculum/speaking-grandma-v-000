dbc6ebfdf02da602cd3d0bbb2575210a88666b9d
Charity Youngblood <youngblood.charity@gmail.com>
2017-11-01 19:09:08 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(string)
 # Whatever you say to grandma, she should respond with
-
-  if string.downcase
+  if string === "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif string.downcase
     return "HUH?! SPEAK UP, SONNY!"
 
   elsif string.uppercase
