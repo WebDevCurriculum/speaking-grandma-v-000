e7a8892c9ceb653c6c52f55dcdaa848aa3e65eca
Charity Youngblood <youngblood.charity@gmail.com>
2017-11-01 19:06:22 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@ def speak_to_grandma(string)
     return "HUH?! SPEAK UP, SONNY!"
   else string.upcase
     return "NO, NOT SINCE 1938!"
-  
+
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 # If you shout, she can hear you (or at least she thinks so)
